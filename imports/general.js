21a65199d360b3734f8609a6ea24d84890a6eb40
Kalle Hansson <kh.hansson@gmail.com>
2018-07-08 16:08:39 +0200

    Updated plugins

--- imports/general.js
+++ imports/general.js
@@ -1,7 +1,6 @@
 module.exports = {
 	extends: ['./globals', './rules'].map(require.resolve),
 	parser: 'babel-eslint',
-	plugins: ['eslint-plugin-jest', 'eslint-plugin-import'].map(require.resolve),
 	env: {
 		'jest/globals': true
 	}
